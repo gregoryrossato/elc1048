9312c3ac7ccd407229e6c0e3fdaae9fdee0a15a3
gregoryrossato <gregory.rossato@acad.ufsm.br>
2021-07-29 11:33:12 -0300

    exercicio_3_sistema_multitarefas_semaforos

--- MULTITAREFAS/IAR/main.c
+++ MULTITAREFAS/IAR/main.c
@@ -10,20 +10,37 @@ void tarefa_1(void);
 void tarefa_2(void);
 void tarefa_3(void);
 void tarefa_4(void);
+void produtor(void);
+void consumidor(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
  */
 #define TAM_PILHA_1		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_2		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_3		(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_4		(TAM_MINIMO_PILHA + 24)
+#define TAM_PRODUTOR		(TAM_MINIMO_PILHA + 24)
+#define TAM_CONSUMIDOR		(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
+#define TAM_BUFFER              10
 
 /*
  * Declaracao das pilhas das tarefas
  */
 uint32_t PILHA_TAREFA_1[TAM_PILHA_1];
 uint32_t PILHA_TAREFA_2[TAM_PILHA_2];
+uint32_t PILHA_TAREFA_3[TAM_PILHA_3];
+uint32_t PILHA_TAREFA_4[TAM_PILHA_4];
+uint32_t PILHA_PRODUTOR[TAM_PRODUTOR];
+uint32_t PILHA_CONSUMIDOR[TAM_CONSUMIDOR];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
+
+semaforo_t cheio = {0, NULL};
+semaforo_t vazio = {TAM_BUFFER, NULL};
+
+uint32_t buffer[TAM_BUFFER];
+uint8_t e=0, f=0, read;
 
 /*
  * Funcao principal de entrada do sistema
@@ -37,6 +54,14 @@ int main(void)
 	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
 	
 	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+        
+        CriaTarefa(tarefa_3, "Tarefa 3", PILHA_TAREFA_3, TAM_PILHA_3, 3);
+        
+        CriaTarefa(tarefa_4, "Tarefa 4", PILHA_TAREFA_4, TAM_PILHA_4, 4);
+        
+        CriaTarefa(produtor, "Produtor", PILHA_PRODUTOR, TAM_PRODUTOR, 5);
+        
+        CriaTarefa(consumidor, "Consumidor", PILHA_CONSUMIDOR, TAM_CONSUMIDOR, 1);
 	
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
@@ -53,6 +78,7 @@ int main(void)
 	}
 }
 
+//semaforo_t SemaforoTeste = [0,0];
 
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
 void tarefa_1(void)
@@ -95,4 +121,32 @@ void tarefa_4(void)
                 TarefaEspera(100);
         }
 }
+
+void produtor(){
+  
+  while(1){ 
+
+    SemaforoAguarda(&vazio);    
+    buffer[e]=500%10;        
+    e++;
+    if(e == TAM_BUFFER){
+      e=0; 
+    }                 
+    SemaforoLibera(&cheio);
+    TarefaEspera(3);
+  }
+}
+
+void consumidor(){
+  
+  while(1){
+    SemaforoAguarda(&cheio);
+    read = buffer[f]; 
+    f++;
+    if(f == TAM_BUFFER){
+      f=0;   
+    }           
+    SemaforoLibera(&vazio);
+  }
+} 
     
